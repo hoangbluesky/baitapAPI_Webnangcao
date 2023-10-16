7d6da133e4bfc2fad4cc5bb2f544f4433d7ba482
HaVanHoang <ngaha1534@gmail.com>
2023-10-16 15:07:03 +0700

    first commit

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+class Database
+{
+
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = "";
+    private $database = "laptrinhweb";
+
+    public function getConnection()
+    {
+
+        $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+
+        if ($conn->connect_error) {
+            die("Error failed to connect to MySQL: " . $conn->connect_error);
+
+        } else {
+            return $conn;
+        }
+
+    }
+}
